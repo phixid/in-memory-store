f361e3bf14a7e6bd2cd7f27f0a77ab2fb9f1f112
Kristof Hermans <kristof.hermans.kh@gmail.com>
2019-03-20 17:28:53 +0100

    build to cjs and es6

---
+++ rollup.config.js
@@ -0,0 +1,67 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+
+const pkg = require('./package');
+
+const input = 'src/index.ts';
+const output = {
+	es: `dist/${pkg.name}.es.js`,
+	cjs: `dist/${pkg.name}.cjs.js`
+};
+const packageName = pkg.name.charAt(0).toUpperCase() + pkg.name.slice(1);
+const banner = `/*  ${packageName} v${pkg.version}
+ *  https://github.com/phixid/${pkg.name}
+ *  (c) ${new Date().getFullYear()} ${pkg.author}
+ *  ${packageName} may be freely distributed under the ${pkg.license} license.
+ */
+`;
+
+const config = {
+	input: input,
+	output: {},
+	plugins: [
+		resolve(),
+		commonjs()
+	]
+};
+
+if (process.env.NODE_ENV === 'ES') {
+	config.plugins.push(
+		typescript({
+			tsconfig: 'tsconfig.json',
+			tsconfigOverride: {
+				compilerOptions: {
+					target: 'es6'
+				}
+			}
+		})
+	);
+	config.output = {
+		banner: banner,
+		file: output.es,
+		format: 'es',
+		name: packageName
+	};
+}
+
+if (process.env.NODE_ENV === 'CJS') {
+	config.plugins.push(
+		typescript({
+			tsconfig: 'tsconfig.json',
+			tsconfigOverride: {
+				compilerOptions: {
+					target: 'es5'
+				}
+			}
+		})
+	);
+	config.output = {
+		banner: banner,
+		file: output.cjs,
+		format: 'cjs',
+		name: packageName
+	};
+}
+
+module.exports = config;
